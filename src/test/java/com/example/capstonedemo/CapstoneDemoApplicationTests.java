2b75344913c5eb5021895ca851a3aee1cb374bc1
duongdd141100 <duongdd141100@gmail.com>
2024-01-08 23:42:16 +0700

    init

---
+++ src/test/java/com/example/capstonedemo/CapstoneDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstonedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
